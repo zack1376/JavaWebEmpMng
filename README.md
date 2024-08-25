7ea6be6e0e0008f57600cfdd780c2e4fb9883fb0
zack <lizq8302@qq.com>
2024-08-25 10:06:15 +0800

    原始代码

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java Web Employee Management
---
+++ src/test/java/com/zack/demoweb2/DemoWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zack.demoweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
